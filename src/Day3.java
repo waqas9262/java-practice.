a9e9dff480f667fad2390cefa7b6beafb5d6ff1e
Waqas Khan <waqasrasheed413@gmail.com>
2025-08-27 04:12:01 +0100

    Done till Day2 Q3

---
+++ src/Day3.java
@@ -0,0 +1,34 @@
+public class Day3 {
+    public static void main(String[] args) {
+//        Write a program to store 5 numbers in an array and print them.
+//        int[] numbers = {34, 22, 43, 94, 15};
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+
+//        Write a program to find the largest number in an array.
+//        int [] numbers = {23,4,45,656,1};
+//        int largest = numbers [0];
+//
+//        for (int i = 1; i<numbers.length; i++){
+//            if (numbers[i] > largest){
+//                largest = numbers[i];
+//            }
+//        }
+//        System.out.println("The largest number in the given array is : " + largest);
+
+//        Write a program to find the smallest number in an array.
+//        int[] numbers = {9, 2, 3, 4, 5};
+//        int smallest = numbers[0];
+//
+//        for (int i = 1; i < numbers.length; i++) {
+//            if (numbers[i] < smallest) {
+//                smallest = numbers[i];
+//            }
+//        }
+//        System.out.println("The largest number in the given array is : " + smallest);
+
+
+    }
+}
