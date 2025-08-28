fd07cd28a2832d0af67a09720933b07090d0e3a2
Waqas Khan <waqasrasheed413@gmail.com>
2025-08-28 18:59:36 +0100

    Done till Day4 Q2

---
+++ src/Day4.java
@@ -0,0 +1,41 @@
+//public class Day4 {
+//    Write a method that takes two numbers and returns the bigger one.
+//    public static int getBigger(int a, int b) {
+//        if (a > b) {
+//            return a;
+//        } else {
+//            return b;
+//        }
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int num1 = 94;
+//        int num2 = 64;
+//
+//        int Bigger = getBigger(num1, num2);
+//        System.out.println("The bigger number is " + Bigger);
+//
+//    }
+//}
+
+
+//public class Day4 {
+//    Write a method that returns the factorial of a number.
+//    public static int getFactorial(int a) {
+//        int result = 1;
+//        for (int i =1; i<=a; i++){
+//            result = result * i;
+//        }
+//        return result;
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        int num = 5;
+//
+//        int Factorial = getFactorial(num);
+//        System.out.println("The factorial is " + Factorial);
+//
+//    }
+//}
