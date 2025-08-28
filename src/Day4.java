d2059c758455e12416a54832933f914306ea4984
Waqas Khan <waqasrasheed413@gmail.com>
2025-08-28 23:48:14 +0100

    Day 4: methods (functions) — bigger number, factorial, palindrome, array sum, Fibonacci

--- src/Day4.java
+++ src/Day4.java
@@ -39,3 +39,68 @@
 //
 //    }
 //}
+
+//public class Day4 {
+//    Write a method to check if a word is a palindrome (like “madam”).
+
+//    public static boolean isPalindrome(String word) {
+//        String reversed = new StringBuilder(word).reverse().toString();
+//        return word.equals(reversed);
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        String TestWord = "madam";
+//        if (isPalindrome(TestWord)) {
+//            System.out.println(TestWord + " is Palindrome");
+//        }
+//        else{
+//            System.out.println(TestWord + " is not Palindrome");
+//        }
+//
+//    }
+//}
+
+
+//public class Day4 {
+//    Write a method to calculate the sum of all elements in an array.
+
+//    public static int getSum(int[] num) {
+//        int sum = 0;
+//        for (int i = 0; i < num.length; i++) {
+//            sum = sum + num[i];
+//        }
+//        return sum;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] array = {20, 20, 20, 20, 20};
+//        int Total = getSum(array);
+//
+//        System.out.println("The sum of the given array is: " + Total);
+//
+//    }
+//}
+
+
+//public class Day4 {
+//    public static void getFibonacci(int n) {
+//        int first = 0;
+//        int second = 1;
+//
+//        System.out.print("Fibonacci series: " );
+//        for (int i=1; i <= n; i++){
+//            System.out.print(first + " ");  // print the current number
+//
+//            int next = first + second;
+//            first = second;
+//            second = next;
+//        }
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        int n = 10;
+//        getFibonacci(n);
+//    }
+//}
